39468bd4ca6441a73a55e05aadb00cbc5a53a3ee
dadi3d <mail@dadi3d.de>
2025-11-11 12:34:32 +0100

    0.1.8b7

--- src/pages/equipment-manager/SetKopieren.jsx
+++ src/pages/equipment-manager/SetKopieren.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { MAIN_VARIABLES } from "../../config";
-import { authenticatedFetch } from "../../utils/authenticatedFetch";
+import { authenticatedFetch } from "../services/auth";
 import "./SetAnlegen.css";
 
 export default function SetKopieren() {
